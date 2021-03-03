6847d90f6b8b3f3c9bb6be2c9abe92b140470399
jiangliang <jiangliang@kuaishou.com>
2021-03-03 14:40:15 +0800

    项目搭建

---
+++ src/main/java/com/server/be_chatting/BeChattingApplication.java
@@ -0,0 +1,13 @@
+package com.server.be_chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeChattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeChattingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/be_chatting/BeChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.be_chatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
