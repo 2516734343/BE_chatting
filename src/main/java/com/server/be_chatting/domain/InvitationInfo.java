f661f3598b55bcab74865571a1c0e5b3b57ce770
jiangliang <jiangliang@kuaishou.com>
2021-03-15 14:13:15 +0800

    开发-done

--- src/main/java/com/server/be_chatting/domain/InvitationInfo.java
+++ src/main/java/com/server/be_chatting/domain/InvitationInfo.java
@@ -1,9 +1,13 @@
 package com.server.be_chatting.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+
 import lombok.Data;
 
 @Data
 public class InvitationInfo {
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
     private String content;
     private Long userId;
--- src/main/java/com/server/be_chatting/domain/InvitationLikeRelation.java
+++ src/main/java/com/server/be_chatting/domain/InvitationLikeRelation.java
@@ -1,9 +1,13 @@
 package com.server.be_chatting.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+
 import lombok.Data;
 
 @Data
 public class InvitationLikeRelation {
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
     private Long invitationId;
     private Long userId;
