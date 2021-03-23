179c05e8204f98e97391dc3ae3e034e2855fdbf1
jiangliang <jiangliang@kuaishou.com>
2021-03-23 19:54:52 +0800

    开发-done

--- src/main/java/com/server/be_chatting/domain/ChatRecordInfo.java
+++ src/main/java/com/server/be_chatting/domain/ChatRecordInfo.java
@@ -1,9 +1,13 @@
 package com.server.be_chatting.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+
 import lombok.Data;
 
 @Data
 public class ChatRecordInfo {
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
     private Long userId;
     private Long targetUserId;
